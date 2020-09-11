82ca92324efbaeeae42894e8e0709ceb571dbdd6
ahmedmchamed <tunashark@gmail.com>
2020-09-11 22:13:38 +0100

    fleshing out container state

--- front_end_form/src/containers/form.js
+++ front_end_form/src/containers/form.js
@@ -19,6 +19,30 @@ class ToyForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleCustomerName(event) {
+        this.setState({
+            customerName: event.target.value
+        })
+    }
+
+    handleCustomerEmail(event) {
+        this.setState({
+            customerEmail: event.target.value
+        })
+    }
+
+    handleCustomerPhoneNumber(event) {
+        this.setState({
+            customerPhoneNumber: event.target.value
+        })
+    }
+
+    handleCustomerAddress(event) {
+        this.setState({
+            customerAddress: event.target.value
+        })
+    }
+
     handleSubmit() {
 
     }
@@ -26,19 +50,21 @@ class ToyForm extends Component {
     render() {
         return (
             <>
+            <div class="toy-form">
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="customer-name">Customer name</label>
-                    <input type="text" id="customer-name" name="name" />
+                    <input type="text" id="customer-name" value={this.state.customerName} onChange={this.handleCustomerName} name="name" />
 
                     <label htmlFor="customer-email">Customer email</label>
-                    <input type="text" id="customer-email" name="email" />
+                    <input type="text" id="customer-email" value={this.state.customerEmail} onChange={this.handleCustomerEmail} name="email" />
 
                     <label htmlFor="customer-phone-number">Customer phone number</label>
-                    <input type="text" id="customer-phone-number" name="phone_number" />
+                    <input type="text" id="customer-phone-number" value={this.state.customerPhoneNumber} onChange={this.handleCustomerPhoneNumber} name="phone_number" />
 
                     <label htmlFor="customer-address">Customer address</label>
-                    <input type="text" id="customer-address" name="address" />
+                    <input type="text" id="customer-address" value={this.state.customerAddress} onChange={this.handleCustomerAddress} name="address" />
                 </form>
+            </div>
             </>
         )
     }
