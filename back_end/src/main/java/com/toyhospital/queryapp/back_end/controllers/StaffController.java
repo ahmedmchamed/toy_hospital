5b74e535711251931e43ed88a64d95ab23cf64cb
ahmedmchamed <tunashark@gmail.com>
2020-10-06 13:40:19 +0100

    added staff controller

---
+++ back_end/src/main/java/com/toyhospital/queryapp/back_end/controllers/StaffController.java
@@ -0,0 +1,37 @@
+package com.toyhospital.queryapp.back_end.controllers;
+
+import com.toyhospital.queryapp.back_end.models.Staff;
+import com.toyhospital.queryapp.back_end.repositories.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StaffController {
+
+    @Autowired
+    StaffRepository staffRepository;
+
+    @GetMapping(name = "/staff")
+    public ResponseEntity<List<Staff>> getAllStaff() {
+        List<Staff> allStaff = staffRepository.findAll();
+        return new ResponseEntity<List<Staff>>(allStaff, HttpStatus.OK);
+    }
+
+    @GetMapping(name = "/staff/{id}")
+    public ResponseEntity<Optional<Staff>> getStaffById(@PathVariable Long id) {
+        Optional<Staff> staffMember = staffRepository.findById(id);
+        return new ResponseEntity<Optional<Staff>>(staffMember, HttpStatus.OK);
+    }
+
+    @PostMapping(name = "/staff")
+    public ResponseEntity<Staff> postStaff(@RequestBody Staff staff) {
+        Staff newStaffMember = staffRepository.save(staff);
+        return new ResponseEntity<Staff>(newStaffMember, HttpStatus.CREATED);
+    }
+
+}
