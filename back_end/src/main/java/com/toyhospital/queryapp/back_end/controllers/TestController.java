20efce6a2ffcd62ec7a53074a14d2903b4e5e25c
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-10-11 15:29:07 +0100

    Fixed role/roles bug in signup request

--- back_end/src/main/java/com/toyhospital/queryapp/back_end/controllers/TestController.java
+++ back_end/src/main/java/com/toyhospital/queryapp/back_end/controllers/TestController.java
@@ -1,7 +1,10 @@
 package com.toyhospital.queryapp.back_end.controllers;
 
 import com.toyhospital.queryapp.back_end.models.Role;
+import com.toyhospital.queryapp.back_end.models.Toy;
+import com.toyhospital.queryapp.back_end.models.User;
 import com.toyhospital.queryapp.back_end.repositories.RoleRepository;
+import com.toyhospital.queryapp.back_end.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,9 @@ public class TestController {
     @Autowired
     RoleRepository roleRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
@@ -33,6 +39,33 @@ public class TestController {
     public ResponseEntity<Role> postRole(@RequestBody Role role) {
         Role newRole = roleRepository.save(role);
         return new ResponseEntity<>(newRole, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/roles")
+    public void deleteRoles() {
+        roleRepository.deleteAll();
+    }
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> allUsers = userRepository.findAll();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/users")
+    public void deleteUsers() {
+        userRepository.deleteAll();
+    }
+
+    @PutMapping(value = "/user/{:id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id) {
+        User userToUpdate = userRepository.findById(id).get();
+        userToUpdate.setUsername(user.getUsername());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setPassword(user.getPassword());
+        userToUpdate.setRoles(user.getRoles());
+        userRepository.save(userToUpdate);
+        return new ResponseEntity<>(userToUpdate, HttpStatus.OK);
     }
 
     @GetMapping("/user")
