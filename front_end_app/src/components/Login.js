791170c3c06d9243c683854b62f7844be2a53841
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-10-12 15:17:22 +0100

    Refactor login form to use react hook form for form validation

--- front_end_app/src/components/Login.js
+++ front_end_app/src/components/Login.js
@@ -1,84 +1,42 @@
-import React, { useState, useRef } from "react"
-import Form from "react-validation/build/form"
-import Input from "react-validation/build/input"
-import CheckButton from "react-validation/build/button"
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
 
 import AuthService from "../services/auth.service"
 
-const required = (value) => {
-    if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This field is required!
-            </div>
-        )
-    }
-}
-
 const Login = (props) => {
 
-    const form = useRef()
-    const checkBtn = useRef()
+    const { register, handleSubmit, watch, errors } = useForm()
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const [message, setMessage] = useState("")
+    const [loading, setLoading] = useState(false) //To do: Add loading icon!
 
-    const onChangeUsername = (event) => {
-        const username = event.target.value
-        setUsername(username)
-    }
+    const handleLogin = (data) => {
 
-    const onChangePassword = (event) => {
-        const password = event.target.value
-        setPassword(password)
-    }
-
-    const handleLogin = (event) => {
-        event.preventDefault()
-
-        console.log("Button clicked");
-
-        setMessage("")
         setLoading(true)
 
-        form.current.validateAll()
-
-        if (checkBtn.current.context._errors.length === 0) {
-            AuthService.login(username, password).then(
-                () => {
-                    console.log("Yaaay, logged in!");
-                    props.history.push("/home")
-                    window.location.reload()
-                },
-                (error) => {
-                    const resMessage = (error.response && error.response.data && error.response.data.message) ||error.message || error.toString()
-                    console.log("You failed...");
-                    setLoading(false)
-                    setMessage(resMessage)
-                }
-            )
-        } else {
+        AuthService.login(data.username, data.password).then(
+            () => {
+                props.history.push("/home")
+                window.location.reload()
+            }, error => {
+                alert("Wrong password or username, please try again!")
+                setLoading(false);
+            },
             setLoading(false)
-        }
+        )
+
     }
     
     return (
         <div>
-            <Form onSubmit={handleLogin} ref={form}>
+            <form onSubmit={handleSubmit(handleLogin)}>
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" value={username} onChange={onChangeUsername} validations={[required]}/>
+                <input type="text" name="username" placeholder="Username" ref={register({required: true})}/>
+                {errors.username && <span>Required field</span>}
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={password} onChange={onChangePassword} validations={[required]}/>
-                <button disabled={loading}>
-                    {loading && (
-                        <span></span>
-                    )}
-                    <span>Login</span>
-                </button>
-                <CheckButton style={{ display: "none"}} ref={checkBtn}></CheckButton>
-            </Form>
+                <input type="password" name="password" placeholder="Password" ref={register({required: true})}/>
+                {errors.password && <span>Required field</span>}
+                <input type="submit" value="Login"/>
+            </form>
         </div>
     )
 
