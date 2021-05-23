de641edb5837e4cc138ea5e21169b07762031b06
ahmedmchamed <tunashark@gmail.com>
2021-05-23 16:20:30 +0100

    refactored form into a functional component instead

--- front_end_form/src/containers/form.js
+++ front_end_form/src/containers/form.js
@@ -1,90 +1,71 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './form.css';
 
-class ToyForm extends Component {
+const ToyForm = () => {
 
-    constructor(props) {
-        super();
-        this.state = {
-            customerName: null,
-            customerEmail: "jacko@email.com",
-            customerPhoneNumber: "01234567",
-            customerAddress: "100 Somewhere Lane",
-            customerPhotos: [],
-            customerToys: [],
-            toyName: "Fluffles",
-            toyType: "Bear",
-            toyAge: 1,
-            toySize: 1.0,
-            customerRepairDescription: "Fix my fluffles"
-        }
-        this.fileUpload = React.createRef();
+    let fileUpload = React.createRef();
 
-        this.addToy = this.addToy.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleCustomerName = this.handleCustomerName.bind(this);
-        this.handleCustomerEmail = this.handleCustomerEmail.bind(this);
-        this.handleCustomerPhoneNumber = this.handleCustomerPhoneNumber.bind(this);
-        this.handleCustomerAddress = this.handleCustomerAddress.bind(this);
-        this.handleToyName = this.handleToyName.bind(this);
-        this.handleToyType = this.handleToyType.bind(this);
-        this.handleToyAge = this.handleToyAge.bind(this);
-        this.handleToySize = this.handleToySize.bind(this);
-        this.handleRepairDescription = this.handleRepairDescription.bind(this);
-        this.handleFileUpload = this.handleFileUpload.bind(this);
+    const [customerName, setCustomerName] = useState(null);
+    const [customerEmail, setCustomerEmail] = useState("jacko@email.com");
+    const [customerPhoneNumber, setCustomerPhoneNumber] = useState("012345670");
+    const [customerAddress, setCustomerAddress] = useState("100 There Lane");
+    const [customerPhotos, setCustomerPhotos] = useState([]);
+    const [customerToys, setCustomerToys] = useState([]);
+    const [toyName, setToyName] = useState("BearBones");
+    const [toyType, setToyType] = useState("Bear");
+    const [toyAge, setToyAge] = useState(1);
+    const [toySize, setToySize] = useState(1);
+    const [customerRepairDescription, setCustomerRepairDescription] = useState("It's broken. Very useful.");
+
+
+    const handleCustomerName = (event) => {
+        setCustomerName(event.target.value);
     }
 
-    handleCustomerName(event) {
-        this.setState({ customerName: event.target.value })
+    const handleCustomerEmail = (event) => {
+        setCustomerEmail(event.target.value);
     }
 
-    handleCustomerEmail(event) {
-        this.setState({ customerEmail: event.target.value })
+    const handleCustomerPhoneNumber = (event) => {
+        setCustomerPhoneNumber(event.target.value);
     }
 
-    handleCustomerPhoneNumber(event) {
-        this.setState({ customerPhoneNumber: event.target.value })
+    const handleCustomerAddress = (event) => {
+        setCustomerAddress(event.target.value);
     }
 
-    handleCustomerAddress(event) {
-        this.setState({ customerAddress: event.target.value })
+    const handleToyName = (event) => {
+        setToyName(event.target.value);
     }
 
-    handleToyName(event) {
-        this.setState({ toyName: event.target.value })
+    const handleToyType = (event) => {
+        setToyType(event.target.value);
     }
 
-    handleToyType(event) {
-        this.setState({ toyType: event.target.value })
+    const handleToyAge = (event) => {
+        setToyAge(event.target.value);
     }
 
-    handleToyAge(event) {
-        this.setState({ toyAge: event.target.value })
+    const handleToySize = (event) => {
+        setToySize(event.target.value);
     }
 
-    handleToySize(event) {
-        this.setState({ toySize: event.target.value })
+    const handleRepairDescription = (event) => {
+        setCustomerRepairDescription(event.target.value);
     }
 
-    handleRepairDescription(event) {
-        this.setState({ customerRepairDescription: event.target.value })
-    }
-
-    handleFileUpload() {
+    const handleFileUpload = () => {
 
         const allFiles = [
-            ...this.state.customerPhotos,
-            this.fileUpload.current.files
+            ...customerPhotos,
+            fileUpload.current.files
         ]
         
-        this.setState({
-            customerPhotos: allFiles
-        }, () => {
-            console.log(this.state.customerPhotos)
-        })
+        setCustomerPhotos(allFiles);
+        console.log(customerPhotos);
     }
 
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         // event.preventDefault();
         const customerPostUrl = "http://localhost:8080/api/customers";
         const toyPostUrl = "http://localhost:8080/api/toys";
@@ -92,20 +73,20 @@ class ToyForm extends Component {
 
         let files = new FormData();
 
-        for (let i = 0; i < this.state.customerPhotos.length; i++) {
-            for (let j = 0; j < this.state.customerPhotos[i].length; j++) {
-                console.log(this.state.customerPhotos[i][j])
-                files.append(`files[${i}][${j}]`, this.state.customerPhotos[i][j])
+        for (let i = 0; i < customerPhotos.length; i++) {
+            for (let j = 0; j < customerPhotos[i].length; j++) {
+                console.log(customerPhotos[i][j])
+                files.append(`files[${i}][${j}]`, customerPhotos[i][j])
             }
         }
 
         const customerAndToy = {
-            "toys": this.state.customerToys,
+            "toys": customerToys,
             "customer": {
-                "customerName": this.state.customerName,
-                "customerEmail": this.state.customerEmail,
-                "customerPhoneNumber": this.state.customerPhoneNumber,
-                "customerAddress": this.state.customerAddress
+                "customerName": customerName,
+                "customerEmail": customerEmail,
+                "customerPhoneNumber": customerPhoneNumber,
+                "customerAddress": customerAddress
             }
         }
 
@@ -122,90 +103,85 @@ class ToyForm extends Component {
         })
         .then(res => res.json())
         .then(() => {
-            this.setState({customerToys: []})
+            setCustomerToys([]);
         })
     }
     
-    addToy(event) {
+    const addToy = (event) => {
         event.preventDefault()
-        this.setState({
-            customerToys: [...this.state.customerToys, {
-                toyName: this.state.toyName,
-                toyType: this.state.toyType,
-                toyAge: this.state.toyAge,
-                toySize: this.state.toySize,
-                repairFromCustomer: this.state.customerRepairDescription
-            }]
+
+        setCustomerToys([...customerToys, {
+            toyName: toyName,
+            toyType: toyType,
+            toyAge: toyAge,
+            toySize: toySize,
+            repairFromCustomer: customerRepairDescription
+        }])
+        console.log(customerToys)
+    }
+
+    let toyListItems = []
+
+    if (customerToys.length > 0) {
+        toyListItems = customerToys.map((toy, index) => {
+            return (
+                <li key={"toy_" + index}>{toy.toyName}</li>
+            )
         })
-        console.log(this.state.customerToys)
     }
 
-    render() {
+    return (
+        <>
+        <div className="toy-form">
+            <form onSubmit={addToy} encType="multipart/form-data">
+                <label htmlFor="customer-name">Customer name</label>
+                <input type="text" id="customer-name" value={customerName} onChange={handleCustomerName} name="customer_name" />
 
-        let toyListItems = []
+                <label htmlFor="customer-email">Customer email</label>
+                <input type="text" id="customer-email" value={customerEmail} onChange={handleCustomerEmail} name="email" />
 
-        if (this.state.customerToys.length > 0) {
-            toyListItems = this.state.customerToys.map((toy, index) => {
-                return (
-                    <li key={"toy_" + index}>{toy.toyName}</li>
-                )
-            })
-        }
+                <label htmlFor="customer-phone-number">Customer phone number</label>
+                <input type="text" id="customer-phone-number" value={customerPhoneNumber} onChange={handleCustomerPhoneNumber} name="phone_number" />
 
-        return (
-            <>
-            <div className="toy-form">
-            {/*  */}
-                <form onSubmit={this.addToy} encType="multipart/form-data">
-                    <label htmlFor="customer-name">Customer name</label>
-                    <input type="text" id="customer-name" value={this.state.customerName} onChange={this.handleCustomerName} name="customer_name" />
+                <label htmlFor="customer-address">Customer this.ddress</label>
+                <input type="text" id="customer-address" value={customerAddress} onChange={handleCustomerAddress} name="address" />
 
-                    <label htmlFor="customer-email">Customer email</label>
-                    <input type="text" id="customer-email" value={this.state.customerEmail} onChange={this.handleCustomerEmail} name="email" />
+                <div className="toy-details-submission">
+                    <label htmlFor="toy-name">Toy name</label>
+                    <input type="text" id="toy-name" value={toyName} onChange={handleToyName} name="toy_name" />
 
-                    <label htmlFor="customer-phone-number">Customer phone number</label>
-                    <input type="text" id="customer-phone-number" value={this.state.customerPhoneNumber} onChange={this.handleCustomerPhoneNumber} name="phone_number" />
+                    <label htmlFor="toy-type">Toy type</label>
+                    <select value={toyType} defaultValue="default" onChange={handleToyType} name="toy_type">
+                        <option disabled value="default">Please choose your toy type...</option>
+                        <option value="mechanical">Mechanical</option>
+                        <option value="teddy">Teddy</option>
+                        <option value="doll">Doll</option>
+                        <option value="other">Other</option>
+                    </select>
 
-                    <label htmlFor="customer-address">Customer address</label>
-                    <input type="text" id="customer-address" value={this.state.customerAddress} onChange={this.handleCustomerAddress} name="address" />
+                    <label htmlFor="toy-age">Toy age</label>
+                    <input type="number" id="toy-age" value={toyAge} onChange={handleToyAge} name="toy_age" />
 
-                    <div className="toy-details-submission">
-                        <label htmlFor="toy-name">Toy name</label>
-                        <input type="text" id="toy-name" value={this.state.toyName} onChange={this.handleToyName} name="toy_name" />
+                    <label htmlFor="toy-size">Toy size (cm)</label>
+                    <input type="number" id="toy-size" value={toySize} onChange={handleToySize} name="toy_size" />
 
-                        <label htmlFor="toy-type">Toy type</label>
-                        <select value={this.state.toyType} defaultValue="default" onChange={this.handleToyType} name="toy_type">
-                            <option disabled value="default">Please choose your toy type...</option>
-                            <option value="mechanical">Mechanical</option>
-                            <option value="teddy">Teddy</option>
-                            <option value="doll">Doll</option>
-                            <option value="other">Other</option>
-                        </select>
-
-                        <label htmlFor="toy-age">Toy age</label>
-                        <input type="number" id="toy-age" value={this.state.toyAge} onChange={this.handleToyAge} name="toy_age" />
-
-                        <label htmlFor="toy-size">Toy size (cm)</label>
-                        <input type="number" id="toy-size" value={this.state.toySize} onChange={this.handleToySize} name="toy_size" />
-
-                        <label htmlFor="toy-repair-description">Describe the repairs needed</label>
-                        <textarea id="toy-repair-description" value={this.state.customerRepairDescription} onChange={this.handleRepairDescription} name="repair_from_customer" ></textarea>
-                    
-                        <label htmlFor="customer-photo-upload">Upload your photos</label>
-                        <input type="file" name="files" id="customer-photo-upload" ref={this.fileUpload} onChange={this.handleFileUpload} multiple></input>
-                        <input type="submit" value="ADD TOY"/>
-                    </div>
-                </form>
-                <button type="button" onClick={this.handleSubmit}>Submit toys</button>
-            </div>
-            <div>
-                <ul>
-                    {toyListItems}
-                </ul>
-            </div>
-            </>
-        )
-    }
+                    <label htmlFor="toy-repair-description">Describe the repairs needed</label>
+                    <textarea id="toy-repair-description" value={customerRepairDescription} onChange={handleRepairDescription} name="repair_from_customer" ></textarea>
+                
+                    <label htmlFor="customer-photo-upload">Upload your photos</label>
+                    <input type="file" name="files" id="customer-photo-upload" ref={fileUpload} onChange={handleFileUpload} multiple></input>
+                    <input type="submit" value="ADD TOY"/>
+                </div>
+            </form>
+            <button type="button" onClick={handleSubmit}>Submit toys</button>
+        </div>
+        <div>
+            <ul>
+                {toyListItems}
+            </ul>
+        </div>
+        </>
+    )
 
 }
 
